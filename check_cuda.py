6dcc9ae925dfb1ef9d52d61ca8a3bd2c448f67d1
Mas Gendon <67081785+gendonholaholo@users.noreply.github.com>
2024-12-05 13:37:14 +0700

    Update check_cuda.py

--- check_cuda.py
+++ check_cuda.py
@@ -1,5 +1,5 @@
 import torch
-print(torch.__version__)  # Versi PyTorch yang terinstal
-print(torch.cuda.is_available())  # Apakah CUDA tersedia
-print(torch.cuda.get_device_name(0))  # Nama GPU pertama yang terdeteksi
+print(torch.__version__) 
+print(torch.cuda.is_available())  
+print(torch.cuda.get_device_name(0))  
 
