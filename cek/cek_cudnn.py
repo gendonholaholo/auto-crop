f38c5a26fd24ac7ba93a39e873da5db575186249
Mas Gendon <67081785+gendonholaholo@users.noreply.github.com>
2024-12-05 20:00:58 +0700

    Mbohhhhh

---
+++ cek/cek_cudnn.py
@@ -0,0 +1,4 @@
+import torch
+print(torch.backends.cudnn.is_available())  
+print(torch.backends.cudnn.version()) 
+
--- src/crop_background.py
+++ src/crop_background.py
@@ -9,23 +9,21 @@ import torchvision.models.segmentation as models
 def load_deeplabv3_model(model_path="models/model.pth"):
     model = models.deeplabv3_resnet101(pretrained=False)
     
-    state_dict = torch.load(model_path, map_location=torch.device('cpu'))
+    state_dict = torch.load(model_path)  
     
     model.load_state_dict(state_dict)
-    
     model.eval()  
+    
     return model
 
 def segment_image(model, image):
-    input_image = preprocess_image(image, target_size=(513, 513))
+    input_image = preprocess_image(image, target_size=(513, 513))  
     
     input_tensor = torch.from_numpy(input_image).float().unsqueeze(0)
     
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
-    input_tensor = input_tensor.to(device)
+    input_tensor = input_tensor.to(next(model.parameters()).device)  
     
-    with torch.no_grad():  
+    with torch.no_grad():
         output = model(input_tensor)['out'][0]
     
     segmentation_mask = output.argmax(0).cpu().numpy()  
@@ -33,8 +31,14 @@ def segment_image(model, image):
 
 def auto_crop(image, mask):
     indices = np.where(mask > 0)
+    
+    if indices[0].size == 0 or indices[1].size == 0:
+        print("Tidak ada objek yang terdeteksi. Mengembalikan gambar asli.")
+        return image
+    
     top_left = (min(indices[1]), min(indices[0]))
     bottom_right = (max(indices[1]), max(indices[0]))
+    
     cropped_image = image[top_left[1]:bottom_right[1], top_left[0]:bottom_right[0]]
     return cropped_image
 
@@ -48,18 +52,19 @@ def save_output(cropped_image, output_path="output/cropped_image.jpg"):
 
 def main(image_path):
     model_path = "models/model.pth"  
-    model = load_deeplabv3_model(model_path)
+    model = load_deeplabv3_model(model_path)  
 
     image = load_image(image_path)  
-    mask = segment_image(model, image)
+    mask = segment_image(model, image)  
     
-    cropped_image = auto_crop(image, mask)
-    save_output(cropped_image)
-    plot_image(cropped_image)
+    cropped_image = auto_crop(image, mask)  
+    save_output(cropped_image)  
+    plot_image(cropped_image)  
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Auto crop background using DeepLabV3 model in PyTorch")
     parser.add_argument("--image_path", type=str, required=True, help="Path to the image file")
     args = parser.parse_args()
+    
     main(args.image_path)
 
